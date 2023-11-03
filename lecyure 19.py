d7dd5a3df1da7132576b53a0cc40b486c5bf010f
Shreya-2507 <149791519+Shreya-2507@users.noreply.github.com>
2023-11-03 18:06:58 +0530

    code writer

---
+++ lecyure 19.py
@@ -0,0 +1,35 @@
+x = 0
+r = x%2
+
+if r == 1:
+    print("odd")
+
+if  r == 1:
+    print("odd")
+    print("odd")
+if x > 8:
+    print ("greater")
+if x ==9:
+    print(int(9))
+
+
+
+
+x = 5
+if x==1:
+   print("one")
+   
+elif x ==2:
+    print ("TWO")
+    
+elif x==3:
+    print("three")
+    
+else:
+    print("i AM WAITING")
+   
+x = "shreya"
+if x == "ayush":
+    print(int(1))
+else:
+    print ("ayush")
